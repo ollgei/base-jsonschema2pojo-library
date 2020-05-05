b27c33854d5ba021e6c71356efdc4699f886d869
张家伟 <zhangjiawei@zhongruigroup.com>
2020-05-05 22:22:23 +0800

    init

---
+++ src/main/java/com/github/ollgei/base/swagger/annotator/SwaggerAnnotator.java
@@ -0,0 +1,85 @@
+package com.github.ollgei.base.swagger.annotator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.sun.codemodel.JAnnotationUse;
+import com.sun.codemodel.JClass;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JFieldVar;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.jsonschema2pojo.AbstractAnnotator;
+import org.jsonschema2pojo.GenerationConfig;
+
+/**
+ * swagger.
+ * @author ollgei
+ * @since 1.0.0
+ */
+public class SwaggerAnnotator extends AbstractAnnotator {
+
+    private static final String NODE_PROPERTY_DESCRIPTION = "description";
+    private static final String NODE_PROPERTY_REQUIRED = "required";
+    private static final String NODE_PROPERTY_EXAMPLE = "example";
+    private static final String NODE_PROPERTY_ALLOWABLE_VALUES = "allowableValues";
+    private static final String ANNOTATION_PARAM_NAME = "name";
+    private static final String ANNOTATION_PARAM_VALUE = "value";
+    private static final String ANNOTATION_PARAM_REQUIRED = "required";
+    private static final String ANNOTATION_PARAM_DESCRIPTION = "description";
+    private static final String ANNOTATION_PARAM_ALLOWABLE_VALUES = "allowableValues";
+    private static final String ANNOTATION_PARAM_EXAMPLE = "example";
+
+
+    public SwaggerAnnotator(GenerationConfig generationConfig) {
+        super(generationConfig);
+    }
+
+    @Override
+    public void typeInfo(JDefinedClass clazz, JsonNode node) {
+        JAnnotationUse annotation = clazz.annotate(ApiModel.class);
+        if (node.has(NODE_PROPERTY_DESCRIPTION)) {
+            annotation.param(ANNOTATION_PARAM_DESCRIPTION, node.get(NODE_PROPERTY_DESCRIPTION).asText());
+        }
+    }
+
+    @Override
+    public void propertyField(JFieldVar field, JDefinedClass clazz, String propertyName, JsonNode node) {
+
+        JAnnotationUse annotation =
+                modelPropertyAnnotation(field).param(ANNOTATION_PARAM_NAME, propertyName);
+        if (node.has(NODE_PROPERTY_DESCRIPTION)) {
+            annotation.param(ANNOTATION_PARAM_VALUE, node.get(NODE_PROPERTY_DESCRIPTION).asText());
+        }
+        if (node.has(NODE_PROPERTY_REQUIRED)) {
+            annotation.param(ANNOTATION_PARAM_REQUIRED, node.get(NODE_PROPERTY_REQUIRED).asBoolean());
+        }
+
+        if (node.has(NODE_PROPERTY_ALLOWABLE_VALUES)) {
+            modelPropertyAnnotation(field).param(ANNOTATION_PARAM_ALLOWABLE_VALUES,
+                    node.get(NODE_PROPERTY_ALLOWABLE_VALUES).asText());
+        }
+
+        if (node.has(NODE_PROPERTY_EXAMPLE)) {
+            modelPropertyAnnotation(field).param(ANNOTATION_PARAM_EXAMPLE,
+                    node.get(NODE_PROPERTY_EXAMPLE).asText());
+        }
+
+    }
+
+    private JAnnotationUse modelPropertyAnnotation(JFieldVar field) {
+        JClass annotationClass = field.type().owner().ref(ApiModelProperty.class);
+        if (annotationClass == null) {
+            return field.annotate(ApiModelProperty.class);
+        }
+        for (JAnnotationUse annotationUse : field.annotations()) {
+            if (annotationUse.getAnnotationClass().isAssignableFrom(annotationClass)) {
+                return annotationUse;
+            }
+        }
+        return field.annotate(ApiModelProperty.class);
+    }
+
+    @Override
+    public boolean isAdditionalPropertiesSupported() {
+        return false;
+    }
+}
